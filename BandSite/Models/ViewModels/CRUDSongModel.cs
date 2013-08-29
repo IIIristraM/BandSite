4d8ec3eb3b8ea1367ae877403f9c9c39a11087a9
iiiristram <iiiristram@gmail.com>
2013-08-29 15:47:50 +0400

    reload again

---
+++ BandSite/Models/ViewModels/CRUDSongModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace BandSite.Models.ViewModels
+{
+    public class CRUDSongModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Song Title")]
+        [Required]
+        public string Title { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Text { get; set; }
+
+        public HttpPostedFileBase UploadFile { get; set; }
+    }
+}
